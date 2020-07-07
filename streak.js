b1b9fd3002a639e198c3f5dc72552db9f6f7002b
Malik Kennedy <mksybr@gmail.com>
2020-07-06 20:42:55 -0700

    Six is beterr than 9

--- streak.js
+++ streak.js
@@ -1,38 +1,3 @@
-function gridData() {
-  var data = new Array();
-  var xpos = 1; //starting xpos and ypos at 1 so the stroke will show when we make the grid below
-  var ypos = 1;
-  var width = 32;
-  var height = 32;
-  let max = 8 * 4 * 6;
-  // iterate for rows 
-  for (var row = 0; row < 8; row++) {
-    data.push( new Array() );
-
-    // iterate for cells/columns inside rows
-    for (var column = 0; column < (4 * 6); column++) {
-      
-      let today = new Date();
-      today.setDate(today.getDate() - max)
-      max--;
-      data[row].push({
-        x: xpos,
-        y: ypos,
-        width: width,
-        height: height,
-	days_ago: today
-      })
-      // increment the x position. I.e. move it over by 50 (width variable)
-      xpos += width;
-    }
-    // reset the x position after a row is complete
-    xpos = 1;
-    // increment the y position for the next row. Move it down 50 (height variable)
-    ypos += height; 
-  }
-  return data;
-}
-
 function gridData2() {
   var data = new Array();
   var xpos = 1; //starting xpos and ypos at 1 so the stroke will show when we make the grid below
